b90fd0486a7d923731a6ec1d02c045ecea34da8a
liuzhixin <liuzhixin@huorong.cn>
2023-02-20 14:21:20 +0800

    java

---
+++ java_lzx/src/main/java/HelloWord.java
@@ -0,0 +1,10 @@
+/**
+ * @author lzx
+ * @date 2023/2/20 14:20
+ * @description: TODO
+ */
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("hello word");
+    }
+}
