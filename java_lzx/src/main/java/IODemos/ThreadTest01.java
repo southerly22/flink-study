4dbf9c870ab73ad5611ff4c2bea774879e320084
southerly22 <2496663421@qq.com>
2023-03-22 23:14:32 +0800

    java 多线程读文件02

---
+++ java_lzx/src/main/java/IODemos/ThreadTest01.java
@@ -0,0 +1,57 @@
+package IODemos;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * 线程池
+ *
+ * @author lzx
+ * @date 2023/03/22 23:04
+ **/
+public class ThreadTest01 {
+    private static Log logger =  LogFactory.getFactory().getInstance(ThreadTest01.class);
+
+    public static void main(String[] args) {
+        int threadNum = 10;
+        try {
+            ExecutorService threadPool = Executors.newFixedThreadPool(threadNum);
+            for (int i = 0; i < threadNum; i++) {
+                threadPool.execute(new Mytask("lzx"+i,i));
+            }
+            threadPool.shutdown();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    static class Mytask implements Runnable{
+
+        private String name;
+        private int id;
+
+        public Mytask(String name, int id) {
+            this.name = name;
+            this.id = id;
+        }
+
+        @Override
+        public String toString() {
+            return "Mytask{" +
+                    "name='" + name + '\'' +
+                    ", id=" + id +
+                    '}';
+        }
+
+        @Override
+        public void run() {
+            String name = Thread.currentThread().getName();
+            long id = Thread.currentThread().getId();
+            logger.warn("线程名字："+name+"，线程Id： "+ id+","+this.toString());
+        }
+    }
+}
--- java_lzx/src/main/java/IODemos/test02.java
+++ java_lzx/src/main/java/IODemos/test02.java
@@ -7,7 +7,7 @@ package IODemos;
  */
 public class test02 {
     public static void main(String[] args) {
-        long fileSize = 107745;
+        long fileSize = 108745;
         long threadNum = 10;
         long chunkSize = 200;
 
@@ -17,24 +17,15 @@ public class test02 {
         long start = 0L, end = 0L, mid = 0L;
 
         for (long i = 0; i < batchSize; i++) {
+
             mid = i * threadNum * chunkSize;
+            System.out.println(mid);
             for (long l = 0; l < threadNum; l++) {
-                start = l * chunkSize + mid;
-                end = (l + 1) * chunkSize - 1 + mid;
-                System.out.println(start+"=="+end);
-                System.out.println("------------------");
-            }
-            if (i == batchSize -1) {
-                for (long k = 0; k < threadNum; k++) {
-                    start = k * chunkSize + mid;
-                    end += (k + 1) * chunkSize - 1 + mid;
-                    if (k == threadNum-1){
-                        end += remainSize;
-                    }
-                    System.out.println(start+"=="+end);
-                }
+                start = Math.min(l * chunkSize + mid,fileSize);
+                end = Math.min((l + 1) * chunkSize - 1 + mid,fileSize);
+                if (start == fileSize) break; //终止循环
+                System.out.println(start + "==" + end);
             }
         }
-
     }
 }
