b65047cc1a01873980db9c51af4633a8b658a4df
southerly22 <2496663421@qq.com>
2023-04-27 09:47:34 +0800

    flink-java:waterwark_Api01

---
+++ flink-java/src/main/java/flink_core/sink/WaterMark_Api_01.java
@@ -0,0 +1,43 @@
+package flink_core.sink;
+
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+import java.time.Duration;
+
+/**
+ * @author lzx
+ * @date 2023/04/26 18:19
+ **/
+public class WaterMark_Api_01 {
+    public static void main(String[] args) throws Exception {
+        Configuration configuration = new Configuration();
+        configuration.setInteger("rest.port", 8085);
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(configuration);
+
+        DataStreamSource<String> stream = env.socketTextStream("localhost", 9999);
+
+        // 策略4： WatermarkStrategy.forMonotonousTimestamps()   单调，紧跟最大事件时间
+        // 策略3： WatermarkStrategy.forGenerator()              自定义watermark 生成策略
+        // 策略2： WatermarkStrategy.forBoundedOutOfOrderness()  有界无序，允许乱序watermark生成策略
+        // 策略1： WatermarkStrategy.noWatermarks()              不生成watermark 禁用事件时间的推进机制
+
+        /**
+         * @author lzx
+         * @date 2023-04-26 18:27
+         * 示例一 ：从源头算子生成watermark
+         */
+
+        // 1.构造一个watermark 生成策略对象（算法策略 以及事件时间抽取方法）
+        WatermarkStrategy<String> watermarkStrategy = WatermarkStrategy
+                .<String>forBoundedOutOfOrderness(Duration.ofMillis(0)) //允许乱序的算法策略
+                .withTimestampAssigner((element, recordTimestamp) -> Long.parseLong(element.split(",")[2]));
+
+        // 2.将构造好的watermark 策略对象，分配给流（Source算子）
+        stream.assignTimestampsAndWatermarks(watermarkStrategy);
+
+        env.execute("WaterMark_Api_01");
+    }
+}
