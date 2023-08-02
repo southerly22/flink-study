99ac77eaf23ebd6bc7b1589cb16cb30990325e50
liuzhixin <liuzhixin@huorong.cn>
2023-08-02 10:16:26 +0800

    flink-sgg:代码提交总

--- flink-sgg/src/main/java/dwd/DwdTrafficUserJumpDetail.java
+++ flink-sgg/src/main/java/dwd/DwdTrafficUserJumpDetail.java
@@ -5,19 +5,15 @@ import com.alibaba.fastjson.JSONObject;
 import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
-import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.cep.CEP;
 import org.apache.flink.cep.PatternSelectFunction;
 import org.apache.flink.cep.PatternStream;
 import org.apache.flink.cep.PatternTimeoutFunction;
 import org.apache.flink.cep.pattern.Pattern;
-import org.apache.flink.cep.pattern.conditions.IterativeCondition;
 import org.apache.flink.cep.pattern.conditions.SimpleCondition;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
-import org.apache.flink.formats.json.JsonNodeDeserializationSchema;
-import org.apache.flink.formats.json.JsonRowDataDeserializationSchema;
-import org.apache.flink.formats.json.JsonRowDeserializationSchema;
+import org.apache.flink.streaming.api.CheckpointingMode;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.KeyedStream;
@@ -25,7 +21,6 @@ import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.ProcessFunction;
 import org.apache.flink.streaming.api.windowing.time.Time;
-import org.apache.flink.streaming.util.serialization.JSONKeyValueDeserializationSchema;
 import org.apache.flink.util.Collector;
 import org.apache.flink.util.OutputTag;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
@@ -47,6 +42,8 @@ public class DwdTrafficUserJumpDetail {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
+        env.enableCheckpointing(1000, CheckpointingMode.EXACTLY_ONCE);
+        env.getCheckpointConfig().setCheckpointStorage("file:///D:\\WorkPlace\\flink-study\\flink-sgg\\ck");
 
         KafkaSource<String> kafkaSource = KafkaSource.<String>builder()
                 .setBootstrapServers("192.168.1.56:9092,192.168.1.61:9092,192.168.1.58:9092,192.168.3.71:9092,192.168.3.178:9092")
@@ -60,23 +57,30 @@ public class DwdTrafficUserJumpDetail {
         // todo 1.分配水位线
         DataStreamSource<String> kafkaDS = env.fromSource(
                 kafkaSource,
-                WatermarkStrategy.<String>forBoundedOutOfOrderness(Duration.ofSeconds(2))
-                        .withTimestampAssigner((str, l) -> JSON.parseObject(str).getLong("ts")),
+                WatermarkStrategy.noWatermarks(),
                 "kafkaSource");
-
-        kafkaDS.process(new ProcessFunction<String, String>() {
-            @Override
-            public void processElement(String value, ProcessFunction<String, String>.Context ctx, Collector<String> out) throws Exception {
-                System.out.println("currentWatermark: " + ctx.timerService().currentWatermark());
-                out.collect(value);
-            }
-        }).print();
 
         // todo 2.拿到kafka数据 转为jsonObj，按照mid分组 （设备id分组）
         KeyedStream<JSONObject, String> keyedStream = kafkaDS.map(JSON::parseObject)
+                .assignTimestampsAndWatermarks(
+                        WatermarkStrategy.<JSONObject>forBoundedOutOfOrderness(Duration.ofSeconds(2))
+                                .withTimestampAssigner(new SerializableTimestampAssigner<JSONObject>() {
+                                    @Override
+                                    public long extractTimestamp(JSONObject element, long recordTimestamp) {
+                                        return element.getLong("ts");
+                                    }
+                                })
+                )
                 .keyBy(json -> json.getJSONObject("common").getString("mid"));
 
-        // keyedStream.print();
+        keyedStream.process(new ProcessFunction<JSONObject, String>() {
+            @Override
+            public void processElement(JSONObject value, ProcessFunction<JSONObject, String>.Context ctx, Collector<String> out) throws Exception {
+                System.out.println("currentWatermark: " + ctx.timerService().currentWatermark());
+                out.collect(value.toJSONString());
+            }
+        }).print();
+
 
         // todo 3.定义CEP规则
         Pattern<JSONObject, JSONObject> pattern = Pattern.<JSONObject>begin("start").where(new SimpleCondition<JSONObject>() {
@@ -104,7 +108,8 @@ public class DwdTrafficUserJumpDetail {
         PatternStream<JSONObject> patternDS = CEP.pattern(keyedStream, pattern);
 
         // todo 5.提取事件（匹配上的事件 和 超时事件）
-        OutputTag<String> timeOut = new OutputTag<String>("timeout"){};
+        OutputTag<String> timeOut = new OutputTag<String>("timeout") {
+        };
 
         SingleOutputStreamOperator<String> selectDS = patternDS.select(timeOut, new PatternTimeoutFunction<JSONObject, String>() {
             @Override
@@ -119,12 +124,13 @@ public class DwdTrafficUserJumpDetail {
         });
         DataStream<String> timeoutDS = selectDS.getSideOutput(timeOut);
 
+        selectDS.union(timeoutDS);
         // todo 6.输出2条流
         selectDS.print("select>>>");
         timeoutDS.print("timeout>>>");
 
         // todo 7.合并2条流
-        selectDS.union(timeoutDS);
+        // selectDS.union(timeoutDS);
         env.execute("DwdTrafficUserJumpDetail");
     }
 }
